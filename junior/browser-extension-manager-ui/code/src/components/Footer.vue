e5f97a1f5cc9bda89d77dc9ca7e76b0fed7e1671
Fernando Pérez Mojica <ferperezm95@gmail.com>
2025-04-04 19:34:04 -0600

    Initial commit

---
+++ junior/browser-extension-manager-ui/code/src/components/Footer.vue
@@ -0,0 +1,10 @@
+<script setup>
+  
+</script>
+
+<template>
+  <div class="attribution">
+    Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>. 
+    Coded by <a href="#">Your Name Here</a>.
+  </div>
+</template>
