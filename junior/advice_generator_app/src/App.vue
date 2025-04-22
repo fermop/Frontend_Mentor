a884c8ec0d1c464f59ed89a3a6344491180148eb
Fernando Pérez Mojica <ferperezm95@gmail.com>
2025-04-22 13:20:56 -0600

    advice generator app - starter

---
+++ junior/advice_generator_app/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+  import Footer from './components/Footer.vue';
+</script>
+
+<template>
+  Advice #<!-- Advice ID goes here -->
+
+  "<!-- Advice text goes here -->"
+
+  <Footer class="hidden"/>
+</template>
