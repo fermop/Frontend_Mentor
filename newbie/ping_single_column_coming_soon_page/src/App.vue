154d7e8acaca3e81a251676350c516e72a5b0623
Fernando Pérez Mojica <ferperezm95@gmail.com>
2025-04-30 08:51:11 -0600

    ping_single_column_coming_soon_page - starter

---
+++ newbie/ping_single_column_coming_soon_page/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+  import Footer from './components/Footer.vue';
+</script>
+
+<template>
+
+  We are launching soon!
+
+  Subscribe and get notified
+
+  Notify Me
+
+  &copy; Copyright Ping. All rights reserved.
+
+  <Footer class="hidden"/>
+</template>
