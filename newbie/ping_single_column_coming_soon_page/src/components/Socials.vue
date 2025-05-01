7d9d8f042c85d434cd0889c08f4f8620ad156881
Fernando Pérez Mojica <ferperezm95@gmail.com>
2025-05-01 11:40:58 -0600

    ping_single_column_coming_soon_page - version

---
+++ newbie/ping_single_column_coming_soon_page/src/components/Socials.vue
@@ -0,0 +1,24 @@
+<template>
+  <section
+    class="flex justify-center gap-[1rem]"
+  >
+    <a 
+      href="#"
+      class="w-[2rem] h-[2rem] text-center leading-[2rem] text-primary-blue border-1 border-secondary-pale-blue rounded-full cursor-pointer hover:text-white hover:bg-primary-blue duration-250"
+    >
+      <i class="fa-brands fa-facebook-f"></i>
+    </a>
+    <a 
+      href="#"
+      class="w-[2rem] h-[2rem] text-center leading-[2rem] text-primary-blue border-1 border-secondary-pale-blue rounded-full cursor-pointer hover:text-white hover:bg-primary-blue duration-250"
+    >
+      <i class="fa-brands fa-twitter"></i>
+    </a>
+    <a 
+      href="#"
+      class="w-[2rem] h-[2rem] text-center leading-[2rem] text-primary-blue border-1 border-secondary-pale-blue rounded-full cursor-pointer hover:text-white hover:bg-primary-blue duration-250"
+    >
+      <i class="fa-brands fa-instagram"></i>
+    </a>
+  </section>
+</template>
