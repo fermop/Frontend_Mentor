9d84fcfedb22797f2e8bf35b9919c835205c5ee8
Fernando Pérez Mojica <ferperezm95@gmail.com>
2025-05-06 11:49:03 -0600

    base_apparel_coming_soon_page - starter

---
+++ newbie/base_apparel_coming_soon_page/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Footer from './components/Footer.vue'
+</script>
+
+<template>
+  We're coming soon
+
+  Hello fellow shoppers! We're currently building our new fashion store. 
+  Add your email below to stay up-to-date with announcements and our launch deals.
+
+  Email Address
+  
+  <Footer class="hidden"/>
+</template>
