4bf13c9e1506f89f026388cc8acb0116fa761a5d
Fernando Pérez Mojica <ferperezm95@gmail.com>
2025-05-15 12:17:16 -0600

    newsletter_sign-up - add image section rounded

--- junior/newsletter_sign-up_with_success_message/src/components/Form.vue
+++ junior/newsletter_sign-up_with_success_message/src/components/Form.vue
@@ -17,7 +17,7 @@
   <main
     class="h-[100dvh] w-full max-w-[58.125rem] sm:h-[40rem] bg-custom-white sm:flex flex-row-reverse sm:py-[1rem] sm:pr-[1rem] sm:rounded-3xl sm:overflow-hidden shadow-2xl"
   >
-    <div class="sm:w-[50%] sm:bg-[url(/assets/images/illustration-sign-up-desktop.svg)] bg-cover bg-center bg-no-repeat">
+    <div class="sm:w-[50%] sm:bg-[url(/assets/images/illustration-sign-up-desktop.svg)] bg-cover bg-center bg-no-repeat sm:rounded-2xl">
       <img
         src="/assets/images/illustration-sign-up-mobile.svg"
         alt=""
